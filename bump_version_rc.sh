6702f3ef06f3e7e9b942eb96b7b82aa5d00300c4
Andreis3 <andrei.as3@hotmail.com>
2022-06-28 08:30:48 -0300

    test

--- bump_version_rc.sh
+++ bump_version_rc.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+GIT_TAG=$(git tag --sort=v:refname | tail -1 | grep -Po '(?<=v)[^"]*')
+BASE_LIST=($(echo $GIT_TAG | tr '.' ' '))
+V_MAJOR=${BASE_LIST[0]}
+V_MINOR=${BASE_LIST[1]}
+V_PATCH=${BASE_LIST[2]}
+V_RC=${BASE_LIST[3]}
+
+if [[ "$V_PATCH" == *"-rc"* ]]; then
+  PATCH_LIST=($(echo $V_PATCH | tr '-' ' '))
+  V_PATCH_BASE=${PATCH_LIST[0]}
+  V_PATCH_RC=${PATCH_LIST[1]}
+  echo "Current version : $GIT_TAG"
+  PATCH_VERSION="$V_MAJOR.$V_MINOR.$V_PATCH_BASE"
+  echo "Enter \"patch\" version to [$PATCH_VERSION]:"
+
+fi
